75e34bd0ac8f61935b1d67dee6358aa5c0f2c532
Pavithra199 <85430932+Pavithra199@users.noreply.github.com>
2021-06-06 12:58:03 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1 align="center">Tips for designing a magazine</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>TIPS</h1>
+      <p>Followings are some Tips:</p>
+        <ul>
+          <li>Don’t Be Shy With Your Cover Designs</li>
+          <br />
+          <li>A Single Pop of Color Shouts the Loudest!</li>
+          <br />
+          <li>Spend Time Perfecting Your Contents Page</li>
+          <br />
+          <li>Illustrated Graphics Make Magazines Unique</li>
+          <br />
+          <li>Give a Digital Look to Print Layouts With Infographics</li>
+        </ul>
+    </div>
+  );
+}
+
+export default Note;
